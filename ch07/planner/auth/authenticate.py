3740832bed0da74942b010bef0ce935eb0d20a46
kenifer <139505395+keniferAma@users.noreply.github.com>
2024-04-18 11:34:15 -0500

    import verify_access_token function from the root

--- ch07/planner/auth/authenticate.py
+++ ch07/planner/auth/authenticate.py
@@ -1,4 +1,4 @@
-from auth.jwt_handler import verify_access_token
+from planner.auth.jwt_handler import verify_access_token
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 
