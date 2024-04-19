f71380dc783e92b0b720b08dee11e401e4307663
kenifer <139505395+keniferAma@users.noreply.github.com>
2024-04-18 20:11:42 -0500

    add some information about CORS

--- ch07/planner/main.py
+++ ch07/planner/main.py
@@ -20,7 +20,23 @@ app = FastAPI(lifespan=db_startup)
 
 # register origins
 
-origins = ["*"]
+"""Cross-Origin Resource Sharing (CORS) is an HTTP-header based mechanism that allows a server to 
+indicate any origins (domain, scheme, or port) other than its own from which a browser should permit 
+loading resources.
+so, if we send a request from http://myfrontend.com to http://mybackend.com and we don't have set the 
+header 'Access-Control-Allow-Origin: *'(which allows any domain to access the resource) 
+the client (browser) will refuse to handle the response. (the server response will be blocked by the browser
+due to security policies)"""
+
+
+origins = ["*"] # This is the array with the allowed origins (THE CLIENT, NOT THE SERVER DOMAIN/PORT)
+"""
+origins = [
+“http://packtpub.com”,
+“https://packtpub.com”
+]
+inside we can set the domains allowed, but in our case, we're allowing from anywhere."""
+
 
 app.add_middleware(
     CORSMiddleware,
