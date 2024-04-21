eff2f38e2cf420af1be5f4f6b9b7ca34e6421447
kenifer <139505395+keniferAma@users.noreply.github.com>
2024-04-21 13:37:16 -0500

    add some information related to setuptools

--- ch07/setup.py
+++ ch07/setup.py
@@ -1,3 +1,6 @@
 from setuptools import setup, find_packages
 
 setup(name="ch07", version="0.1", packages=find_packages())
+
+# for executing this file, we must be placed in our root and the execute the command 'pip install -e .' 
+# which -e means 'editable mode'.
