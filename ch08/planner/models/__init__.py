e17fa9fa90d826eed6140d8b282a94a8ce133324
kenifer <139505395+keniferAma@users.noreply.github.com>
2024-04-30 17:56:25 -0500

    add package functions to imports

---
+++ ch08/planner/models/__init__.py
@@ -0,0 +1,4 @@
+from planner.models.users import User, TokenResponse
+from planner.models.events import Event, EventUpdate
+"""All changes made on the packages were because we named 'ch08' as root, but should've been 'planner'"""
+# Everything I imported was brought to the models package.
