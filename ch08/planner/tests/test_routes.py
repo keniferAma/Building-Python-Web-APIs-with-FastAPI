c68f2f9dc7db4a222ceae74fe6e94dc19ff866ac
kenifer <139505395+keniferAma@users.noreply.github.com>
2024-05-10 18:19:14 -0500

    add scope=session to all tests (was necessary)

--- ch08/planner/tests/test_routes.py
+++ ch08/planner/tests/test_routes.py
@@ -1,32 +1,11 @@
 import httpx
 import pytest
 
-from auth.jwt_handler import create_access_token
-from models.events import Event
+from planner.models import Event
 
 
-@pytest.fixture(scope="module")
-async def access_token() -> str:
-    return create_access_token("testuser@packt.com")
-
-
-@pytest.fixture(scope="module")
-async def mock_event() -> Event:
-    new_event = Event(
-        creator="testuser@packt.com",
-        title="FastAPI Book Launch",
-        image="https://linktomyimage.com/image.png",
-        description="We will be discussing the contents of the FastAPI book in this event.Ensure to come with your own copy to win gifts!",
-        tags=["python", "fastapi", "book", "launch"],
-        location="Google Meet"
-    )
-
-    await Event.insert_one(new_event)
-
-    yield new_event
-
-
-@pytest.mark.asyncio
+# @pytest.mark.order(1)  to execute in the order we want
+@pytest.mark.asyncio(scope="session")
 async def test_get_events(default_client: httpx.AsyncClient, mock_event: Event) -> None:
     response = await default_client.get("/event/")
 
@@ -34,7 +13,7 @@ async def test_get_events(default_client: httpx.AsyncClient, mock_event: Event) 
     assert response.json()[0]["_id"] == str(mock_event.id)
 
 
-@pytest.mark.asyncio
+@pytest.mark.asyncio(scope="session")
 async def test_get_event(default_client: httpx.AsyncClient, mock_event: Event) -> None:
     url = f"/event/{str(mock_event.id)}"
     response = await default_client.get(url)
@@ -42,9 +21,10 @@ async def test_get_event(default_client: httpx.AsyncClient, mock_event: Event) -
     assert response.status_code == 200
     assert response.json()["creator"] == mock_event.creator
     assert response.json()["_id"] == str(mock_event.id)
+    assert response.json()['title'] == 'FastAPI Book Launch'
 
 
-@pytest.mark.asyncio
+@pytest.mark.asyncio(scope="session")
 async def test_post_event(default_client: httpx.AsyncClient, access_token: str) -> None:
     payload = {
         "title": "FastAPI Book Launch",
@@ -74,17 +54,17 @@ async def test_post_event(default_client: httpx.AsyncClient, access_token: str) 
     assert response.json() == test_response
 
 
-@pytest.mark.asyncio
+@pytest.mark.asyncio(scope="session")
 async def test_get_events_count(default_client: httpx.AsyncClient) -> None:
     response = await default_client.get("/event/")
 
     events = response.json()
-
+    print(events)
     assert response.status_code == 200
     assert len(events) == 2
 
 
-@pytest.mark.asyncio
+@pytest.mark.asyncio(scope="session")
 async def test_update_event(default_client: httpx.AsyncClient, mock_event: Event, access_token: str) -> None:
     test_payload = {
         "title": "Updated FastAPI event"
@@ -96,6 +76,7 @@ async def test_update_event(default_client: httpx.AsyncClient, mock_event: Event
     }
 
     url = f"/event/{str(mock_event.id)}"
+    print(mock_event.id)
 
     response = await default_client.put(url, json=test_payload, headers=headers)
 
@@ -103,7 +84,8 @@ async def test_update_event(default_client: httpx.AsyncClient, mock_event: Event
     assert response.json()["title"] == test_payload["title"]
 
 
-@pytest.mark.asyncio
+
+@pytest.mark.asyncio(scope="session")
 async def test_delete_event(default_client: httpx.AsyncClient, mock_event: Event, access_token: str) -> None:
     test_response = {
         "message": "Event deleted successfully."
@@ -122,9 +104,36 @@ async def test_delete_event(default_client: httpx.AsyncClient, mock_event: Event
     assert response.json() == test_response
 
 
-@pytest.mark.asyncio
+@pytest.mark.asyncio(scope="session")
 async def test_get_event_again(default_client: httpx.AsyncClient, mock_event: Event) -> None:
     url = f"/event/{str(mock_event.id)}"
     response = await default_client.get(url)
 
     assert response.status_code == 404
+
+
+
+"""
+-pip install coverage 
+-coverage run -m pytest = to run everything
+-coverage report = to see the results, SO WE MUST EXECUTE 'coverage run -m pytest'
+-pip install pytest-ordering = installation of ordering, pytests library to set the order which modules are 
+executed. (@pytest.mark.run(order=<number of order, starting from 1>))
+"""
+
+
+"""
+IF THE FIXTURE SCOPE WERE SET AS session THE mock_event ONLY EXECUTES ON INSTANCE FOR ALL THE TESTS (IN ALL MODULES)
+THAT MEANS, THAT IN THE DATABASE ONLY WILL BE CREATED ONE EVENT FOR ALL THE TESTS . 
+IN OUR CASE (scope=function) WE'RE CREATING A EVENT FOR EVERY SINGLE TEST, AND THAT'S THE REASON WHY 
+WE'RE NOT GETTING THE DESIRED OUTCOMES. AND OF COURSE WE'RE WASTING TIME CREATING AN INSTANCE FOR EVERY TEST.
+
+THE BEHAVIOR WOULD BE THE SAME WITH module, BUT IF WE HAVE THE TESTS SPREADED FOR SEVERAL MODULES, THAT WILL CAUSE
+ERROR, BECAUSE THIS SCOPE ONLY APPLIES TO THE CURRENT MODULE
+"""
+
+
+"""pytest -vv <file> = run the test """
+
+
+"""THE ASYNCHRONOUS TESTS MUST ALSO HAVE THE RESPECTIVE SCOPE AAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHH"""
